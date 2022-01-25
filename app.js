c9c20378f965c99dd8f57b668d826ddc84b8e8f1
JoshuahBoardman <joshuahboardman@gmail.com>
2022-01-24 19:54:56 -0600

    finished the base html and css

---
+++ app.js
@@ -0,0 +1,6 @@
+const form = document.getElementById("translation-form");
+const input = document.getElementById("form-text-area");
+const output = document.getElementById("translation-output");
+
+
+fo
