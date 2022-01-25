29d17752988e7fb69c1fa26de76f5970fb7efa23
JoshuahBoardman <joshuahboardman@gmail.com>
2022-01-24 23:43:39 -0600

    Finished the js

--- app.js
+++ app.js
@@ -2,5 +2,33 @@ const form = document.getElementById("translation-form");
 const input = document.getElementById("form-text-area");
 const output = document.getElementById("translation-output");
 
+function englishToPigLatin() {
+    let inputToWords = input.value.split(' ');
+    const wordToPigLatin = inputToWords.map(word => {
+        const regex = new RegExp(/[aeiou]/i);
+        let indexOfFirstVowel = word.search(regex);
+        if(indexOfFirstVowel === 0){
+            word = `${word}ay`
+            return word;
+        } else {
+            let wordStart = word.substring(0, indexOfFirstVowel);
+            let wordBody = word.substring(indexOfFirstVowel);
+            word = `${wordBody}${wordStart}ay`;
+            return word;
+        }
+    });
+    const pigLatinString = wordToPigLatin.join(" ");
+    return pigLatinString;
+};
 
-fo
+function displayPigLatin(string) {
+    output.innerText = string;
+}
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    
+    const pigLatinString = englishToPigLatin();
+    displayPigLatin(pigLatinString);
+    input.value = "";
+});
